6ad2a358d007420f5e890d915eaa13ced4a10fdc
lucasz-student <l.hsieh.student@gmail.com>
2023-06-20 16:56:57 -0500

    jaxb unmarshalling and annotations

--- JDBC/DatabasesPractice/src/main/java/com/laba/solvd/database/DAO/Student.java
+++ JDBC/DatabasesPractice/src/main/java/com/laba/solvd/database/DAO/Student.java
@@ -3,11 +3,23 @@ package com.laba.solvd.database.DAO;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Student {
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name = "Student")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Student {
+	
+	@XmlAttribute
 	private int id;
+	@XmlElement(name = "yearJoined")
 	private int yearJoined;
+	@XmlElement(name = "name")
 	private String name;  
+	@XmlElement(name = "major")
 	private String major;
 	
 	private Gym gym;
@@ -106,5 +118,10 @@ public class Student {
 	
 	public void removeUniClass(UniClass UniClass) {
 		this.UniClasses.remove(UniClass);
+	}
+	
+	@Override
+	public String toString() {
+		return "" + this.id + " " + this.name + " " + this.major + " " + this.yearJoined;
 	}
 }
